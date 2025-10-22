c8d677c8bd1401099eeee5c185a39af51f23a518
bumbleby0 <callumcollyer89@gmail.com>
2025-10-22 15:26:09 +0100

    Initial commit

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        Debug.Log("Current Scene Name: " + currentScene.name);
+    }
+
+}
+
